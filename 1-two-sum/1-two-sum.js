2ed0d13e6e0b852a5ec1e4fdce38c69b2bbc4836
Abdulrhman Ashraf <89071774+AbdulrahmanAshraf98@users.noreply.github.com>
2022-05-22 07:19:04 +0200

    Time: 102 ms (59.32%), Space: 42.1 MB (88.99%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -5,7 +5,6 @@
  */
 var twoSum = function(nums, target) {
     const arraySize=nums.length;
-    let resualt=[]
     for(let i=0;i<arraySize;i++){
     const numbertofind=target-nums[i];
     for(let j=i+1;j<arraySize;j++)
