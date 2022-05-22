feaf1765a4ba0e49605146957d19164c7daa46f9
Abdulrhman Ashraf <89071774+AbdulrahmanAshraf98@users.noreply.github.com>
2022-05-22 11:01:58 +0200

    Time: 246 ms (17.34%), Space: 50.7 MB (71.86%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.js
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -3,21 +3,17 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    const arraySize=nums.length;
-    let constIsDuplicate=false;
-    for(let i=0 ;i<arraySize ;i++){
-        for(let j=i+1;j<arraySize ;j++){
-            if(nums[i]===nums[j])
-            constIsDuplicate= true;
-            if(constIsDuplicate){
-                break;
-            }
+    const arraysize=nums.length;
+    let isduplicate=false;
+    nums.sort();
+    for(let i=0 ; i<arraysize;i++)
+        {
+        let nextelement=i+1;
+           if(nums[i]==nums[nextelement])
+               {
+                   isduplicate=true;
+                            break;
+               }
         }
-        if(constIsDuplicate)
-            {
-                break;
-            }
-      
-    }
-    return constIsDuplicate
-};
+     return isduplicate;
+}
