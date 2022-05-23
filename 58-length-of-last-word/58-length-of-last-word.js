5b0057b8f82ef7e7f16cbe721d97eb041ee645b9
Abdulrhman Ashraf <89071774+AbdulrahmanAshraf98@users.noreply.github.com>
2022-05-23 06:02:52 +0200

    Time: 86 ms (33.80%), Space: 43.6 MB (5.74%) - LeetHub

---
+++ 58-length-of-last-word/58-length-of-last-word.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    
+    let words=  s.split(' ');
+    words=words.filter(function(str) {
+    return /\S/.test(str);});
+    const lestWord= words.length-1;
+    const lestWordLength=words[lestWord].length
+    console.log(words);
+    return lestWordLength;
+};
