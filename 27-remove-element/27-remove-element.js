353876f319cf18887fed37116b7348d4d6bb0324
Abdulrhman Ashraf <89071774+AbdulrahmanAshraf98@users.noreply.github.com>
2022-05-23 06:38:35 +0200

    Time: 120 ms (5.60%), Space: 42.4 MB (23.88%) - LeetHub

--- 27-remove-element/27-remove-element.js
+++ 27-remove-element/27-remove-element.js
@@ -5,11 +5,9 @@
  */
 var removeElement = function(nums, val) {
     for (let i=0;i<=nums.length;i++) {
-if (nums[i]===val) {
-nums.splice(i,1);
-i--;
-}
-
-}
-
+        if (nums[i]===val) {
+            nums.splice(i,1);
+            i--;
+            }
+        }
 };
