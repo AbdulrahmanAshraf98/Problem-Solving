7036d6120fdad4469173aff744f68e028d7003a3
Abdulrhman Ashraf <89071774+AbdulrahmanAshraf98@users.noreply.github.com>
2022-07-19 23:59:03 +0200

    Time: 80 ms (73.94%), Space: 45 MB (69.32%) - LeetHub

--- 704-binary-search/704-binary-search.js
+++ 704-binary-search/704-binary-search.js
@@ -4,29 +4,17 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    const middleIndex=Math.ceil(nums.length/2);
-    const middleElement=nums[middleIndex];
-    console.log(middleElement);
-    if(nums.length===1 &&target===nums[0]){
-        return 0;
-    }
-    if(target===middleElement){
-        return middleIndex
-    }
-    else if(target<middleElement){
-        for(let i=0;i<middleIndex;i++){
-            if(target===nums[i]){
-                return i;
-              } 
-        }
-    }
-    else{
-         for(let i=middleIndex+1;i<nums.length;i++){
-            if(target===nums[i]){
-                return i;
-              } 
-             
-        }
-    }
-    return -1
+const arraySize = nums.length;
+	let start = 0;
+	let end = arraySize - 1;
+	let middle = Math.floor((start + end) / 2);
+	while (nums[middle] !== target && start <= end) {
+		if (nums[middle] < target) {
+			start = middle + 1;
+		} else {
+			end = middle - 1;
+		}
+		middle = Math.floor((start + end) / 2);
+	}
+	return nums[middle] === target ? middle : -1;
 };
