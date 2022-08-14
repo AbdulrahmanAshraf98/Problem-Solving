4746affe7cb9d315e6a2d4c0f230058505ff7f21
Abdulrhman Ashraf <89071774+AbdulrahmanAshraf98@users.noreply.github.com>
2022-08-14 10:04:45 +0200

    Time: 165 ms (33.94%), Space: 42.4 MB (75.40%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -3,18 +3,21 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-    const arraySize=nums.length;
-    for(let i=0;i<arraySize;i++){
-    const numbertofind=target-nums[i];
-    for(let j=i+1;j<arraySize;j++)
-        {
-          if(numbertofind===nums[j])
-              {
-                  return[i,j]
-              }
-        }
-        
-    }
-    return null ;
+var twoSum = function(array, target) {
+   	let result = [];
+	const arrSize = array.length - 1;
+	for (let i = 0; i < arrSize; i++) {
+        const findNumber=target-array[i];
+		for (let j = i+1; j <=arrSize; j++) {	
+			if (findNumber === array[j]) {
+				result = [i, j];
+				break;
+			}
+			
+		}
+        if (result.length===2) {
+				break;
+			}
+	}
+	return result;
 };
