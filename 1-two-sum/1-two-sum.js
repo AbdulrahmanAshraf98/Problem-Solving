d77f5f3d3a59ac23e7f2c3ecb93704c07767b01e
Abdulrhman Ashraf <89071774+AbdulrahmanAshraf98@users.noreply.github.com>
2022-08-14 10:11:01 +0200

    Time: 153 ms (38.18%), Space: 42.5 MB (61.09%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -11,10 +11,12 @@ var twoSum = function(array, target) {
 		for (let j = i+1; j <=arrSize; j++) {	
 			if (findNumber === array[j]) {
 				result = [i, j];
-				return result;
+				break;
 			}	
 		}
-
+			if (result.length===2) {
+				break;
+			}
 	}
 	return result;
 };
