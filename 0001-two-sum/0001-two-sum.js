909a24eb390c7264c6801e773fa8dd4ded26e0cb
Abdulrhman Ashraf <89071774+AbdulrahmanAshraf98@users.noreply.github.com>
2022-10-23 04:12:40 +0200

    Time: 75 ms (93.69%), Space: 42.7 MB (45.64%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,6 +4,19 @@
  * @return {number[]}
  */
 var twoSum = function(array, target) {
+//    	let result = [];
+// 	const arrSize = array.length - 1;
+// 	for (let i = 0; i < arrSize; i++) {
+//         const findNumber=target-array[i];
+// 		for (let j = i+1; j <=arrSize; j++) {	
+// 			if (findNumber === array[j]) {
+// 				return [i, j];
+				
+// 			}	
+// 		}
+			
+// 	}
+// 	return null;
     const hashMap={};
     for(let i =0 ; i<array.length;i++){
         if(hashMap[array[i]]>=0){
