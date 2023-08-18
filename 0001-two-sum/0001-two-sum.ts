b4ad9e21cef410474aaaa32a2d2042edf2aec428
Abdulrhman Ashraf <89071774+AbdulrahmanAshraf98@users.noreply.github.com>
2023-08-18 16:47:04 +0300

    Time: 57 ms (92.81%), Space: 45.1 MB (46.25%) - LeetHub

--- 0001-two-sum/0001-two-sum.ts
+++ 0001-two-sum/0001-two-sum.ts
@@ -1,18 +1,14 @@
 function twoSum(nums: number[], target: number): number[] {
-    const sum={}
-    
-   
-     
-     
-      for(let i=0;i<=nums.length;i++){
+    const hashMap={}
+    for(let i=0;i<=nums.length;i++){
          const number=nums[i];
-         let total=target-number;
-      if(sum[number]>=0){
+         let numberToFind=target-number;
+      if(hashMap[number]>=0){
          
-          return [sum[number],i]
+          return [hashMap[number],i]
       }
       else{     
-        sum[total]=i;
+        hashMap[numberToFind]=i;
       }   
       }
      
