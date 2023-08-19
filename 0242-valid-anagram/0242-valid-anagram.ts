59818b808d11091d79cedf19e25d72722fd56666
Abdulrhman Ashraf <89071774+AbdulrahmanAshraf98@users.noreply.github.com>
2023-08-19 18:41:51 +0300

    Time: 77 ms (72.92%), Space: 46.8 MB (42.3%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.ts
+++ 0242-valid-anagram/0242-valid-anagram.ts
@@ -1,6 +1,6 @@
 function isAnagram(s: string, t: string): boolean {
     let angramCharacter={}
-    let isAnagram=true;
+    let isAnagram:boolean=true;
     if(s.length!==t.length){
      isAnagram=false
     }
@@ -17,8 +17,8 @@ function isAnagram(s: string, t: string): boolean {
         if(!angramCharacter[character]){
             isAnagram=false
         }
-        if(angramCharacter[character]){
-            angramCharacter[character]=angramCharacter[character]-1
+        else{
+        angramCharacter[character]=angramCharacter[character]-1
         }
     })
     }
