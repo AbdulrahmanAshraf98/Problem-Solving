d8a60668b119213683466c8dab5f3987262e28fd
Abdulrhman Ashraf <89071774+AbdulrahmanAshraf98@users.noreply.github.com>
2023-08-20 21:17:11 +0300

    Time: 65 ms (38.34%), Space: 42.7 MB (72.58%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.ts
+++ 0392-is-subsequence/0392-is-subsequence.ts
@@ -1,9 +1,25 @@
 function isSubsequence(s: string, t: string): boolean {
-    let i=0;
-    t.split("").forEach(character=>{
-        if(character===s[i]){
+//    let subsequenceCharacter={};
+//    let subSequence="";
+//    s.split("").forEach(character=>subsequenceCharacter[character]=character);
+//    t.split("").forEach(character=>{
+//        if(subsequenceCharacter[character]){
+
+//            subSequence+=character;
+
+//        }
+//    })
+//    console.log(subSequence)
+//    if(s.length!==subSequence.length){
+//        return false
+//    }
+//    return s===subSequence;
+    const sequenceLength=s.length
+    let i = 0 ;
+    for(let j=0;j<=t.length;j++){
+        if(i<sequenceLength&&s[i]===t[j]){
             i++;
         }
-    })
-    return i===s.length
+    }
+    return i===sequenceLength
 };
